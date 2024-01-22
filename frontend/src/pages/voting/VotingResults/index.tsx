7e04e4a9cb39cc47fca56839805884aac5d71d15
NaruebeTh1 <b6419615@g.sut.ac.th>
2024-01-23 03:24:12 +0700

    Add Page Voting Results

---
+++ frontend/src/pages/voting/VotingResults/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+export default function CreateVoting() {
+//   const navigate = useNavigate();
+//   const [data, setData] = useState<interface[]>([]);
+  
+//   const get0 = async () => {
+//     let res = await Get0();
+//     if (res) {
+//       setData(res);
+//     }
+//   };
+
+//   useEffect(() => {
+    
+//   }, []);
+  
+    return (
+        <div>
+            Voting results
+        </div>
+    );
+}
